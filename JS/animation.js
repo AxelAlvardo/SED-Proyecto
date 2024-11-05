8e6276908a38022c05eebe9a8d5d2684a5f05a4e
AxelAlvardo <00216022@uca.edu.sv>
2024-11-04 22:19:40 -0600

    Finished Landing Page

---
+++ JS/animation.js
@@ -0,0 +1,66 @@
+window.sr = ScrollReveal();
+
+sr.reveal('.hero__title', {
+    duration: 1500,
+    origin: 'bottom',
+    distance: '-100px',
+    easing: 'ease'
+});
+
+sr.reveal('.hero__copy', {
+    duration: 1500,
+    origin: 'bottom',
+    distance: '-50px',
+    easing: 'ease',
+    delay: 1000
+});
+
+sr.reveal('.hero__bg', {
+    duration: 1000
+})
+
+sr.reveal('.it-1', {
+    duration: 2000,
+    origin: 'bottom',
+    distance: '-100px'
+});
+sr.reveal('.it-2', {
+    duration: 2300,
+    origin: 'bottom',
+    distance: '-100px'
+});
+sr.reveal('.it-3', {
+    duration: 2500,
+    origin: 'bottom',
+    distance: '-100px'
+});
+sr.reveal('.it-4', {
+    duration: 2700,
+    origin: 'bottom',
+    distance: '-100px'
+});
+
+sr.reveal('.bn-1', {
+    duration: 2000,
+    origin: 'left',
+    distance: '200px'
+})
+
+sr.reveal('.bn-2', {
+    duration: 2500,
+    origin: 'right',
+    distance: '200px'
+})
+
+sr.reveal('.bn-3', {
+    duration: 2800,
+    origin: 'left',
+    distance: '200px'
+})
+
+sr.reveal('.bn-4', {
+    duration: 2500,
+    origin: 'right',
+    distance: '200px'
+})
+
